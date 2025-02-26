8b44d0aa10c7011198267a37cbcdcee3d1b50e33
Emily <emily.zy.lin@gmail.com>
2025-02-26 10:29:25 -0600

    update Factory-method

--- OOD/3. Factory/factory-method/Client.java
+++ OOD/3. Factory/factory-method/Client.java
@@ -1,11 +1,17 @@
 public class Client {
 
     public static void main(String[] args) {
-        Car new_audi = new AudiFactory().getCar();
-        Car new_BYD = new BYDFactory().getCar();
+        //方法一（更优化）：
+        Factory audi_factory = new AudiFactory();
+        Car audi_car = audi_factory.getCar();
+        audi_car.run();
 
-        new_audi.run();
-        new_BYD.run();
+        //方法二：
+        //Car new_audi = new AudiFactory().getCar();
+        // Car new_BYD = new BYDFactory().getCar();
+
+        // new_audi.run();
+        // new_BYD.run();
     }
 
 }
